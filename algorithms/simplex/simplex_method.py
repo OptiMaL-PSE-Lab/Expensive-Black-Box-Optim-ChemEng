ade69fa30a67f39c12232ffbd12bd64d8787134e
tomrsavage <tom.savage@hotmail.com>
2021-01-12 22:48:17 +0000

    simplex-algorithm: commented code, added con store

--- algorithms/simplex/simplex_method.py
+++ algorithms/simplex/simplex_method.py
@@ -1,39 +1,85 @@
 import numpy as np 
-import matplotlib.pyplot as plt 
-from itertools import combinations,product
-import sys
-sys.path.insert(1, 'test_functions')
-from quadratic_constrained import * 
-from rosenbrock_constrained import * 
-
     
 def simplex_method(f,x0,bounds,max_iter,constraints):
-    iterations = max_iter
-    d = len(x0) # dimension 
-    f_range = (bounds[:,1] - bounds[:,0])*0.1 # range of initial simplex
-    x_nodes = np.random.normal(x0,f_range,(d+1,d)) # creating nodes
-    f_nodes = np.zeros((len(x_nodes[:,0]),1)) # function value at each node
-    f_eval_count = 0 
-    con_weight = 1000
-    f_store = np.array([])
-    x_store = np.array([x_nodes[0,:]])
+    '''
+    INPUTS
+    ------------------------------------
+    f:          function to be optimised
+    
+    x0:         initial guess in form [x1,x2,...,xn]
+    
+    bounds:     variable upper and lower bounds in form
+                [[x1l,x1u],[x2l,x2u],...,[xnl,xnu]]
+                
+    max_iter:   total optimisation iterations due to 
+                no stopping conditions
+    
+    constraints: constraint functions in form [g1,g2,...,gm]
+                for m constraint functions.
+                all have form g(x) <= 0 and return g(x)
+                
+    OUTPUTS
+    ------------------------------------
+    output_dict: 
+        - 'x'           : final input variable in R^n
+        
+        - 'f'           : final function value
+        
+        - 'g'           : value of each constraint function at optimal solution
+        
+        - 'f_evals'     : total number of function evaluations
+        
+        - 'f_store'     : best function value at each iteration
+                            
+        - 'x_store'     : list of all previous best variables (per iteration)
+                            
+        - 'g_store'     : list of all previous constraint values (per iteration)
+        
+        - 'g_viol'      : total constraint violation (sum over constraints)
+    
+    NOTES
+    --------------------------------------
+     - Only function values that contribute towards the optimisation are counted.
+     - Stored function values at each iteration are not the penalised objective
+        but the objective function itself.
+    '''
+    bounds = np.array(bounds)   # converting to numpy if not 
+    d = len(x0)                 # dimension 
+    con_d = len(constraints)
+    f_range = (bounds[:,1] - bounds[:,0])*0.1       # range of initial simplex
+    x_nodes = np.random.normal(x0,f_range,(d+1,d))  # creating nodes
+    f_nodes = np.zeros((len(x_nodes[:,0]),1))       # function value at each node
+    f_eval_count = 0            # initialising total function evaluation counter
+    con_weight = 10000           # setting constraint penalty
+    f_store = np.zeros(max_iter)      # initialising function store
+    g_store = np.zeros((max_iter,con_d))
+    x_store = np.zeros((max_iter,d)) # initialising x_store
     # evaluating function 
     for i in range(d+1):
         f_nodes[i,:] = f(x_nodes[i,:]) 
+        # evaulating constraints as penalty
         for ii in range(len(constraints)):
             f_nodes[i,:] += con_weight * max(0,constraints[ii](x_nodes[i,:]))
         f_eval_count += 1 
-    for its in range(1,iterations+1):
+    for its in range(max_iter):
         sorted_nodes = np.argsort(f_nodes[:,0])
         best_nodes = x_nodes[sorted_nodes[:-1]]
-        x_store = np.append(x_store,[x_nodes[sorted_nodes[0]]],axis=0)
-        f_store = np.append(f_store,min(f_nodes))
+        
+        # storing important quantities
+        best_node = x_nodes[sorted_nodes[0]]
+        x_store[its,:] = best_node
+        f_store[its] = f(best_node)
+        con_it = [0 for i in range(len(constraints))]
+        for i in range(len(con_it)):
+            con_it[i] = constraints[i](best_node)
+        g_store[its,:] = con_it 
 
         # centroid of all bar worst nodes
         centroid = np.mean(best_nodes,axis=0)
         # reflection of worst node
         x_reflected = centroid + (centroid - x_nodes[sorted_nodes[-1],:])
         f_reflected =  f(x_reflected) 
+
         for ii in range(len(constraints)):
             f_reflected += con_weight * max(0,constraints[ii](x_reflected))
         f_eval_count += 1 
@@ -70,14 +116,17 @@ def simplex_method(f,x0,bounds,max_iter,constraints):
     for i in range(len(constraints)):
         con_val = constraints[i](x_nodes[sorted_nodes[0]])
         con_viol[i] = con_val 
-            
+    con_viol_total = sum(max(0,con_viol[i]) for i in range(len(con_viol)))         
     output_dict = {}
     output_dict['x'] = x_nodes[sorted_nodes[0]]
     output_dict['f'] = f(x_nodes[sorted_nodes[0]])
-    output_dict['cons_val'] = con_viol 
+    output_dict['g'] = con_viol 
+    output_dict['g_viol'] =  con_viol_total
+    output_dict['g_store'] = g_store
     output_dict['x_store'] = x_store
     output_dict['f_store'] = f_store 
     output_dict['f_evals'] = f_eval_count
+    
     
     return output_dict
 
--- algorithms/simplex/simplex_test.py
+++ algorithms/simplex/simplex_test.py
@@ -11,7 +11,8 @@ g2 = rosenbrock_g2
 d = 2
 its = 50 
 bounds = np.array([[-1.5,1.5],[-0.5,0.5]])
-x0 = [0.0,0.0]
+x0 = [1.0,1.0]
 
 sol = simplex_method(f,x0,bounds,max_iter=its,constraints=[g1,g2])
-print(sol)
+
+print(sol) 
