ffe54c22201c3ef4b191a2fd0032e464bd8320b0
tomrsavage <tom.savage@hotmail.com>
2021-01-13 15:23:52 +0000

    nesterov_random: added algorithm and test file w/ git ignore addition

---
+++ algorithms/nesterov_random/nesterov_random.py
@@ -0,0 +1,73 @@
+import numpy as np 
+
+
+
+def nesterov_random(f,x0,bounds,max_iter,constraints):
+    d = len(x0)
+    con_d = len(constraints)
+    x_store = np.zeros((max_iter,d))
+    g_store = np.zeros((max_iter,con_d))
+    f_store = np.zeros(max_iter)
+    x = x0
+    f_eval_count = 0 
+    alpha = 0.01
+    mu = 0.1
+    con_weight = 10000
+    for i in range(max_iter):
+        x_store[i,:] = x
+        f_store[i] = f(x)
+        
+        u = np.random.normal(0,1,(1,d))
+        B = np.array([[1,0],[0,1]])
+        forw = (x + mu * u)[0,:]
+        back = (x - mu * u)[0,:]
+        
+        f_forw = f(forw)
+        f_eval_count += 1
+        for i in range(len(constraints)):
+            f_forw += con_weight * max(0,constraints[i](forw))
+            
+        f_back = f(back)
+        f_eval_count += 1
+        for i in range(len(constraints)):
+            f_back += con_weight * max(0,constraints[i](back))
+            
+        g = ((f_forw - f_back)/(2*mu))*B@u.T
+        x = x - alpha * (g.T)[0,:]
+        
+   # computing final constraint violation 
+    con_viol = [0 for i in range(len(constraints))]
+    act_cons = [0 for i in range(len(constraints))]
+    for i in range(len(constraints)):
+        con_val = constraints[i](x)
+        con_viol[i] = con_val 
+    con_viol_total = sum(max(0,con_viol[i]) for i in range(len(con_viol)))         
+    output_dict = {}
+    output_dict['x'] = x
+    output_dict['f'] = f(x)
+    output_dict['g'] = con_viol 
+    output_dict['g_viol'] =  con_viol_total
+    output_dict['g_store'] = g_store
+    output_dict['x_store'] = x_store
+    output_dict['f_store'] = f_store 
+    output_dict['f_evals'] = f_eval_count
+    return output_dict
+    
+
+import sys
+sys.path.insert(1, 'test_functions')
+import numpy as np 
+from quadratic_constrained import * 
+from rosenbrock_constrained import * 
+
+f = rosenbrock_f
+g1 = rosenbrock_g1
+g2 = rosenbrock_g2
+d = 2
+its = 50 
+bounds = np.array([[-1.5,1.5],[-0.5,0.5]])
+x0 = [0.5,0.5]
+
+sol = nesterov_random(f,x0,bounds,max_iter=its,constraints=[g1,g2])
+
+print(sol) 
