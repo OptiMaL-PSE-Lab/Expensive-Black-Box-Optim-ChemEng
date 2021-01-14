94fcaf6fa8b3925a063362189d2b1944c1a5b700
tomrsavage <tom.savage@hotmail.com>
2021-01-14 09:30:14 +0000

    nesterov_random: added initial nesterov_random code

--- algorithms/nesterov_random/nesterov_random.py
+++ algorithms/nesterov_random/nesterov_random.py
@@ -53,21 +53,3 @@ def nesterov_random(f,x0,bounds,max_iter,constraints):
     output_dict['f_evals'] = f_eval_count
     return output_dict
     
-
-import sys
-sys.path.insert(1, 'test_functions')
-import numpy as np 
-from quadratic_constrained import * 
-from rosenbrock_constrained import * 
-
-f = rosenbrock_f
-g1 = rosenbrock_g1
-g2 = rosenbrock_g2
-d = 2
-its = 50 
-bounds = np.array([[-1.5,1.5],[-0.5,0.5]])
-x0 = [0.5,0.5]
-
-sol = nesterov_random(f,x0,bounds,max_iter=its,constraints=[g1,g2])
-
-print(sol) 
