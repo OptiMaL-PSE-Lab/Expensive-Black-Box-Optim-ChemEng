88c408a89b9f755ef4b327a5f928c8eedef542fe
tomrsavage <tom.savage@hotmail.com>
2021-01-18 22:49:39 +0000

    nesterov_random: changes to algorithm

--- algorithms/nesterov_random/nesterov_random.py
+++ algorithms/nesterov_random/nesterov_random.py
@@ -1,8 +1,11 @@
 import numpy as np 
-
+import sys
+sys.path.insert(1, 'utilities')
+from general_utility_functions import PenaltyFunctions
 
 
 def nesterov_random(f,x0,bounds,max_iter,constraints):
+    f_aug = PenaltyFunctions(f,constraints,type_penalty='l2',mu=100)
     d = len(x0)
     con_d = len(constraints)
     x_store = np.zeros((max_iter,d))
@@ -10,46 +13,31 @@ def nesterov_random(f,x0,bounds,max_iter,constraints):
     f_store = np.zeros(max_iter)
     x = x0
     f_eval_count = 0 
-    alpha = 0.01
-    mu = 0.1
-    con_weight = 10000
+    alpha = 0.001
+    mu = 0.01
     for i in range(max_iter):
+
         x_store[i,:] = x
-        f_store[i] = f(x)
+        f_store[i] = f_aug.f(x)
         
         u = np.random.normal(0,1,(1,d))
         B = np.array([[1,0],[0,1]])
         forw = (x + mu * u)[0,:]
         back = (x - mu * u)[0,:]
         
-        f_forw = f(forw)
+        f_forw = f_aug.aug_obj(forw)
         f_eval_count += 1
-        for i in range(len(constraints)):
-            f_forw += con_weight * max(0,constraints[i](forw))
             
-        f_back = f(back)
+        f_back = f_aug.aug_obj(back)
         f_eval_count += 1
-        for i in range(len(constraints)):
-            f_back += con_weight * max(0,constraints[i](back))
             
         g = ((f_forw - f_back)/(2*mu))*B@u.T
         x = x - alpha * (g.T)[0,:]
         
-   # computing final constraint violation 
-    con_viol = [0 for i in range(len(constraints))]
-    act_cons = [0 for i in range(len(constraints))]
-    for i in range(len(constraints)):
-        con_val = constraints[i](x)
-        con_viol[i] = con_val 
-    con_viol_total = sum(max(0,con_viol[i]) for i in range(len(con_viol)))         
     output_dict = {}
-    output_dict['x'] = x
-    output_dict['f'] = f(x)
-    output_dict['g'] = con_viol 
-    output_dict['g_viol'] =  con_viol_total
     output_dict['g_store'] = g_store
     output_dict['x_store'] = x_store
     output_dict['f_store'] = f_store 
-    output_dict['f_evals'] = f_eval_count
+    output_dict['N_evals'] = f_eval_count
     return output_dict
     
