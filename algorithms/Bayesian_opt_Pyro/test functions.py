7958ddb490f8c09873205a1b7fe9d2c50c86e872
Panos <ppetsag@gmail.com>
2021-01-20 14:33:54 +0000

    update the test fun for testing pybobyqa

--- algorithms/Bayesian_opt_Pyro/test functions.py
+++ algorithms/Bayesian_opt_Pyro/test functions.py
@@ -8,6 +8,8 @@ import numpy as np
 import pyro
 import pyro.contrib.gp as gp
 from algorithms.Bayesian_opt_Pyro.utilities_full import BayesOpt
+from test_functions import rosenbrock_constrained, quadratic_constrained
+
 from utilities.general_utility_functions import PenaltyFunctions
 assert pyro.__version__.startswith('1.5.1')
 pyro.enable_validation(True)  # can help with debugging
@@ -18,53 +20,12 @@ import functools
 
 
 
-def rosenbrock_f(x):
-    '''
-    Unconstrained Rosenbrock function (objective)
-    '''
-    return (1 - x[0])**2 + 100*(x[1] - x[0]**2)**2
-
-def rosenbrock_g1(x):
-    '''
-    Rosenbrock cubic constraint
-    g(x) <= 0
-    '''
-    return (x[0] - 1)**3 - x[1] + 1
-
-
-def rosenbrock_g2(x):
-    '''
-    Rosenbrock linear constraint
-    g(x) <= 0
-    '''
-    return x[0] + x[1] - 1.8
-
-def penalized_objective(f, g, mu, x):
-    '''
-    :param x: decision variables
-    :type x:  numpy array
-    :param f: Objective
-    :type f:  python function
-    :param g: constraints
-    :type g: list with python functions
-    :return: objective
-    :rtype:  numpy array
-    '''
-    obj   = f(x)
-    n_con = len(g)
-    for i in range(n_con):
-        obj += mu*max(g[i](x),0)**2
-
-    return obj
 
 
 
-
-
-
-f1 = rosenbrock_f
-g1 = rosenbrock_g1
-g2 = rosenbrock_g2
+f1 = rosenbrock_constrained.rosenbrock_f
+g1 = rosenbrock_constrained.rosenbrock_g1
+g2 = rosenbrock_constrained.rosenbrock_g2
 
 Penaly_fun = PenaltyFunctions(f1,[g1,g2],type_penalty='l2', mu=1e3)
 f_pen = Penaly_fun.aug_obj#functools.partial(penalized_objective,f1,[g1,g2], 100)
@@ -82,22 +43,8 @@ soln = pybobyqa.solve(f_pen, x0, bounds=bounds.T)
 solution = BayesOpt().solve(f1, x0, acquisition='EIC',bounds=bounds.T, print_iteration=True, constraints=[g1, g2], casadi=True)
 
 
-
-
-def quadratic_g(x):
-    '''
-    test constraint
-    g(x) <= 0
-    '''
-    return 1 - x[0] - x[1]
-
-def quadratic_f(x):
-    '''
-    test objective
-    '''
-    return x[0]**2 + 10 * x[1]**2 + x[0] * x[1]
-f1 = quadratic_f
-g1 = quadratic_g
+f1 = quadratic_constrained.quadratic_f
+g1 = quadratic_constrained.quadratic_g
 
 f_pen = functools.partial(penalized_objective,f1,[g1,g2], 100)
 
