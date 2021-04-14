89879e997ad967ee9e59b10ba4278c4b4fc0923d
Panos <ppetsag@gmail.com>
2021-04-14 16:36:53 +0100

    Update compose_problem.py

--- case_studies/self_optimization/compose_problem.py
+++ case_studies/self_optimization/compose_problem.py
@@ -1,28 +1,28 @@
 from case_studies.self_optimization import systems
 import numpy as np
 
-def RTO(x):
+def self_opt(x):
     # x = extract_FT(x)
     plant = systems.Static_PDE_reaction_system()
     f = plant.objective
     g1 = plant.constraint_agg_1
     return f(x), [g1(x)]
 
-def RTO_noiseless(x):
+def self_opt_noiseless(x):
     # x = extract_FT(x)
     plant = systems.Static_PDE_reaction_system()
     f = plant.objective_noise_less
     g1 = plant.constraint_agg_1_noiseless
     return f(x), [g1(x)]
 
-def RTO_end_point_constraint(x):
+def self_opt_end_point_constraint(x):
     # x = extract_FT(x)
     plant = systems.Static_PDE_reaction_system()
     f = plant.objective
     g1 = plant.constraint1
     return f(x), [g1(x)]
 
-def RTO_end_point_constraint_noiseless(x):
+def self_opt_end_point_constraint_noiseless(x):
     # x = extract_FT(x)
     plant = systems.Static_PDE_reaction_system()
     f = plant.objective_noise_less
@@ -37,10 +37,10 @@ X        = np.array([[0., 1.], [0.1, 1.], [0., 0.9]])#np.array([[0., 0.], [0.1, 
 xo       = np.array([0.0,0.85])#np.array([0.0,0.05])
 
 
-print('RTO: ', RTO([0,0.85]))
-print('RTO_end_point_constraint', RTO_end_point_constraint([0,0.85]))
-print('RTO_end_point_constraint_noiseless', RTO_end_point_constraint_noiseless([0,0.85]))
-print('RTO_noiseless', RTO_noiseless([0,0.85]))
+print('self_opt: ', self_opt([0,0.85]))
+print('self_opt_end_point_constraint', self_opt_end_point_constraint([0,0.85]))
+print('self_opt_end_point_constraint_noiseless', self_opt_end_point_constraint_noiseless([0,0.85]))
+print('self_opt_noiseless', self_opt_noiseless([0,0.85]))
 
 print(2)
 
